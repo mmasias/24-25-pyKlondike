7526df48cc712a6a28d5b6a1ed63941879b6b44c
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-30 19:46:17 +0200

    feat: clase menu finalizada (pendiente de revisar compatibilidad demas clases)

--- src/doaltoBienvenido/Menu.java
+++ src/doaltoBienvenido/Menu.java
@@ -1,15 +1,48 @@
 package doaltoBienvenido;
 
+import java.util.Scanner;
+
 public class Menu {
 
+    private Mensajes mensaje;
+    private Scanner scanner;
+
+    private String[] opciones = {
+            "1. Mover de Baraja a Descarte",
+            "2. Mover de Descarte a Palo",
+            "3. Mover de Descarte a Columna",
+            "4. Mover de Palo a Columna",
+            "5. Mover de Columna a Palo",
+            "6. Mover de Columna a Columna",
+            "7. Voltear carta de Columna",
+            "8. Voltear Descarte en Baraja",
+            "9. Salir"
+    };
+
+    private final int MIN_OPCIONES = 1;
+    private final int MAX_OPCIONES = 9;
+
+    public Menu() {
+        mensaje = new Mensajes();
+        scanner = new Scanner(System.in);
+    }
+
     public void mostrarOpciones() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrarOpciones'");
+        mensaje.mostrarMensajeLn("OPCIONES -->");
+        for (int i = 0; i < opciones.length; i++) {
+            mensaje.mostrarMensajeLn("  " + opciones[i]);
+        }
     }
 
-    public void elegirOpcion() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'elegirOpcion'");
-    }
+    public int elegirOpcion() {
+        mensaje.mostrarMensajeLn("Elije una opción (1-9): ");
+        int respuestaOpcion = scanner.nextInt();
 
+        while (respuestaOpcion < MIN_OPCIONES || respuestaOpcion > MAX_OPCIONES) {
+            mensaje.mostrarMensajeLn("Opción inválida. Intente de nuevo.");
+            mensaje.mostrarMensajeLn("Elije una opción (1-9): ");
+            respuestaOpcion = scanner.nextInt();
+        }
+        return respuestaOpcion;
+    }
 }
