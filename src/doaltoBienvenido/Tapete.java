2ebd812e320971dc331e4ed31bba6cbc52594b7b
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-30 19:57:39 +0200

    feat: tapete finalizado, a actualizar?

--- src/doaltoBienvenido/Tapete.java
+++ src/doaltoBienvenido/Tapete.java
@@ -1,10 +1,43 @@
 package doaltoBienvenido;
 
 public class Tapete {
+    
+    private Baraja baraja;
+    private Descarte descarte;
+    private Palo[] palos;
+    private Columna[] columnas;
+    private Mensajes mensaje;
+    
+    private final int NUM_PALOS = 4;
+    private final int NUM_COLUMNAS = 7;
+
+    public Tapete(){
+        baraja = new Baraja();
+        descarte = new Descarte();
+        palos = new Palo[NUM_PALOS];
+        columnas = new Columna[NUM_COLUMNAS];
+        mensaje = new Mensajes();
+    }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        mensaje.mostrarMensajeLn("BARAJA: " + baraja);
+        mensaje.mostrarMensajeLn("Descarte: " + descarte);
+        for (int i = 0; i < NUM_PALOS; i++) {
+            mensaje.mostrarMensaje((i + 1) + "º Palo: ");
+            if (palos[i] != null) {
+                System.out.println(palos[i]);
+            } else {
+                mensaje.mostrarMensajeLn("No hay cartas en el palo");
+            }
+        }
+        for (int i = 0; i < NUM_COLUMNAS; i++) {
+            mensaje.mostrarMensaje("Columna [" + (i + 1) + "]: ");
+            if (columnas[i] != null) {
+                System.out.println(columnas[i]);
+            } else {
+                mensaje.mostrarMensajeLn("Columna Vacia");
+            }
+        }
     }
 
 }
