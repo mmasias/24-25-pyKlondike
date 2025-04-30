590673f7448f97756d537eff7ae726609c8d8346
marcof2wow <marcoefigueroa04@wowcenterhn.com>
2025-04-30 03:01:26 +0200

    Refactor Waste class methods to use ArrayList indexing and add method to retrieve all cards

--- entregas/figueroaMarco/src/Waste.java
+++ entregas/figueroaMarco/src/Waste.java
@@ -16,17 +16,21 @@ public class Waste {
     }
 
     public Card getTopCard() {
-        return cards.isEmpty() ? null : cards.getLast();
+        return cards.isEmpty() ? null : cards.get(cards.size() - 1);
     }
 
     public Card removeTopCard() {
-        return cards.isEmpty() ? null : cards.removeLast();
+        return cards.isEmpty() ? null : cards.remove(cards.size() - 1);
     }
 
     public List<Card> removeAllCards() {
         List<Card> temp = new ArrayList<>(cards);
         cards.clear();
         return temp;
+    }
+
+    public List<Card> getAllCards() {
+        return new ArrayList<>(cards);
     }
 
     public boolean isEmpty() {
