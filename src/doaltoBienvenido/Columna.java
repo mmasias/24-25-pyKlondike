ddf4306bce0945fb022fcadb18512cdacc327921
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-05-01 11:30:33 +0200

    feat: clase columna finalizada

--- src/doaltoBienvenido/Columna.java
+++ src/doaltoBienvenido/Columna.java
@@ -2,4 +2,40 @@ package doaltoBienvenido;
 
 public class Columna {
 
+    private Carta[] cartas;
+    private int indiceCarta;
+    private final int MAX_CARTAS_POR_COLUMNA = 19;
+
+    public Columna(){
+        cartas = new Carta[MAX_CARTAS_POR_COLUMNA]; 
+        indiceCarta = 0;
+    }
+
+    public void agregarCarta(Carta carta) {
+        if (indiceCarta < cartas.length) {
+            cartas[indiceCarta++] = carta;
+        }
+    }
+
+    public Carta sacarCarta() {
+        if (indiceCarta > 0) {
+            return cartas[--indiceCarta];
+        }
+        return null;
+    }
+
+    public void darVueltaUltimaCarta() {
+        if (indiceCarta > 0 && !cartas[indiceCarta - 1].esVisible()) {
+            cartas[indiceCarta - 1].darVuelta();
+        }
+    }
+
+    public String toString() {
+        String resultado = "";
+        
+        for (int i = 0; i < indiceCarta; i++) {
+            resultado += cartas[i].toString();
+        }
+        return resultado;
+    }
 }
