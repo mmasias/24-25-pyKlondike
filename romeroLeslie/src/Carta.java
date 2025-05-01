7ae2c5e8a721bb9e2001682d1feb1bdf3f648625
leslieromeroa <leslie.romero@alumnos.uneatlantico.es>
2025-05-01 23:32:45 +0200

    Clase carta finalizada

--- romeroLeslie/src/Carta.java
+++ romeroLeslie/src/Carta.java
@@ -3,5 +3,33 @@ public class Carta {
     private final String palo;
     private boolean visible;
 
+    public Carta(String valor, String palo) {
+        if (valor == null || palo == null) {
+            throw new IllegalArgumentException("Valor y palo no pueden ser nulos.");
+        }
+        this.valor = valor;
+        this.palo = palo;
+        this.visible = false;
+    }
 
+    public void voltear() {
+        visible = !visible;
+    }
+
+    public boolean esVisible() {
+        return visible;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    @Override
+    public String toString() {
+        return visible ? "[" + valor + " " + palo + "]" : "[? ?]";
+    }
 }
