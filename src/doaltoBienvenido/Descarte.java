530423a6bd1a6cfe60c29c61e77962311fd6456f
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-05-01 11:45:15 +0200

    feat: Clase descarte finalizada

--- src/doaltoBienvenido/Descarte.java
+++ src/doaltoBienvenido/Descarte.java
@@ -2,4 +2,35 @@ package doaltoBienvenido;
 
 public class Descarte {
 
+    private Carta[] cartas;
+    private int indiceCarta;
+    private final int CARTAS_TOTALES = 52;
+
+    public Descarte() {
+        cartas = new Carta[CARTAS_TOTALES];
+        indiceCarta = 0;
+    }
+
+    public void agregarCarta(Carta carta) {
+        if (indiceCarta < cartas.length) {
+            carta.darVuelta(); 
+            cartas[indiceCarta++] = carta;
+        }
+    }
+
+    public Carta sacarCarta() {
+        if (indiceCarta > 0) {
+            return cartas[--indiceCarta];
+        }
+        return null;
+    }
+
+    public String toString() {
+        String resultado = "";
+        
+        for (int i = 0; i < indiceCarta; i++) {
+            resultado += cartas[i].toString();
+        }
+        return resultado;
+    }
 }
