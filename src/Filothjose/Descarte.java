42af4e3ccc0d769cc335bd1422ca57524f2ccb59
Jose <jose.filoth@uneatlantico.es>
2025-05-01 17:03:02 +0200

    Descarte

---
+++ src/Filothjose/Descarte.java
@@ -0,0 +1,40 @@
+package Filothjose;
+
+public class Descarte {
+
+    private Carta[] cartas;
+    private int cantidad;
+    private final int CARTAS_TOTALES = 52;
+
+    public Descarte() {
+        cartas = new Carta[CARTAS_TOTALES];
+        cantidad = 0;
+    }
+
+    public void agregarCarta(Carta carta) {
+        if (cantidad < cartas.length) {
+            carta.darVuelta(); 
+            cartas[cantidad++] = carta;
+        }
+    }
+
+    public Carta sacarCarta() {
+        if (cantidad > 0) {
+            return cartas[--cantidad];
+        }
+        return null;
+    }
+
+    public boolean tieneCartas() {
+        return cantidad > 0;
+    }
+
+    public String toString() {
+        String resultado = "";
+
+        for (int i = 0; i < cantidad; i++) {
+            resultado += cartas[i].toString();
+        }
+        return resultado;
+    }
+}
