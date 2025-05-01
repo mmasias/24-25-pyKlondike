a327ee89734cd83a77b9a1e0609de4407787b8df
carlos-alvarado-25 <carlos251005.alvarado@gmail.com>
2025-05-02 00:54:29 +0200

    Añadida clase Fundacion para diferenciar entre montón de Palos y tipo de Palo

---
+++ Fundacion.java
@@ -0,0 +1,9 @@
+
+public class Fundacion {
+
+    public int numeroCartas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'numeroCartas'");
+    }
+
+}
