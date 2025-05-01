7316dc290f983d9304c25604eb62781a27bce617
carlos-alvarado-25 <carlos251005.alvarado@gmail.com>
2025-05-02 00:55:14 +0200

    Implementacion de clase Palo, constructor y toString que devuelve el simbolo del tipo de palo

--- Palo.java
+++ Palo.java
@@ -1,9 +1,31 @@
-
 public class Palo {
 
-    public int numeroCartas() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'numeroCartas'");
+    private String denominacion;
+    private String simbolo;
+
+    public Palo(String denominacion, String simbolo) {
+        this.denominacion = denominacion;
+        this.simbolo = simbolo;
+    }
+
+    public String simbolo(){
+        return simbolo;
+    }
+
+    
+
+    public String toString(){
+        if (denominacion == "Corazones"){
+            return "♥️";
+        } else if (denominacion == "Diamantes"){
+            return "♦️";
+        } else if (denominacion == "Tréboles"){
+            return "♣️";
+        } else if (denominacion == "Picas") {
+            return "♠️";
+        } else {
+            return null;
+        }
     }
 
 }
