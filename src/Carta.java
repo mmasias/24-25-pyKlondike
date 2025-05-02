9e38b164a3b38133309b87ea3daaea4a32776936
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-05-02 16:43:54 +0200

    Implementar la clase Carta con métodos para mostrar y voltear cartas

--- src/Carta.java
+++ src/Carta.java
@@ -1,18 +1,49 @@
 
 public class Carta {
 
+    final private int PALO;
+    final private int VALOR;
+    private boolean visible;
+
+    final String[] palos = {"♥", "♦", "♣", "♠"};
+
     public Carta(int palo, int valor) {
-        //TODO Auto-generated constructor stub
+        this.PALO = palo;
+        this.VALOR = valor;
+        this.visible = false;
     }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        Utilidades utilidades = new Utilidades();
+        if (visible) {
+            utilidades.imprimir("[" + parsePalo() + " " + parseValor() + "]");
+        } else {
+            System.out.println("Carta oculta");
+        }
+    }
+
+    private String parsePalo() {
+        return switch (PALO) {
+            case 0 -> palos[0];
+            case 1 -> palos[1];
+            case 2 -> palos[2];
+            case 3 -> palos[3];
+            default -> "Error";
+        };
     }
 
     public void voltear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'voltear'");
+        visible = !visible;
+    }
+
+    private String parseValor() {
+        return switch (VALOR) {
+            case 1 -> "A";
+            case 11 -> "J";
+            case 12 -> "Q";
+            case 13 -> "K";
+            default -> String.valueOf(VALOR);
+        };
     }
 
 }
