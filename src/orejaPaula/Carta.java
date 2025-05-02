32960298f3545792cb0b9fe548df2997b167309a
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-05-02 11:06:56 +0200

    C,D,T,P

--- src/orejaPaula/Carta.java
+++ src/orejaPaula/Carta.java
@@ -39,10 +39,10 @@ public class Carta {
         if (!estaBocaArriba) return "[? ?]";
         String[] valores = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String simbolo = switch (palo) {
-            case CORAZONES -> "corazon";
-            case DIAMANTES -> "diamante";
-            case TREBOLES -> "trebol";
-            case PICAS -> "pica";
+            case CORAZONES -> "C";
+            case DIAMANTES -> "D";
+            case TREBOLES -> "T";
+            case PICAS -> "P";
         };
         return "[" + valores[valor - 1] + " " + simbolo + "]";
     }
