0538a129c1307ffc4ecc93a9db1a611b8b69bbbb
manuel@aulaInformatica <manuel@masiasweb.com>
2025-05-02 12:08:21 +0200

    Clase Baraja concluida (de momento...)
    Se incluye la clase colaboradora que surgió durante la implementación de la baraja: Carta

---
+++ Carta.java
@@ -0,0 +1,18 @@
+
+public class Carta {
+
+    public Carta(int palo, int numero) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void mostrar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+    }
+
+    public void voltear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'voltear'");
+    }
+
+}
