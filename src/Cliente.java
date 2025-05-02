1b56d16a892f4ab4e3828f00df3a2bb270cd2ef7
Javier Liaño <javier.liano@alumnos.uneatlantico.es>
2025-05-02 08:30:00 +0000

    Modificar método para iniciar el juego en la clase Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,6 +2,6 @@ public class Cliente {
 
     public static void main(String[] args) {
         Klondike klondike = new Klondike();
-        klondike.iniciarJuego();
+        klondike.jugar();
     }
 }
