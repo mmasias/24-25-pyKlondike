61d16eedc2dcc23bc2a159ad581a1200e39d41a6
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-05-04 17:19:38 +0200

    Terminado Utilidades

---
+++ src/Utilidades.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+
+public class Utilidades {
+
+        public static void mostrar(String string) {
+                System.out.print(string);
+        }
+
+        public static String leerString() {
+                Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine();
+                scanner.close();
+                return input;
+        }
+
+        public static int leerInt() {
+                Scanner scanner = new Scanner(System.in);
+                int input = scanner.nextInt();
+                scanner.close();
+                return input;
+        }
+
+        public static int leerInt(int max) {
+                int respuesta;
+                do { 
+                    respuesta = leerInt();
+                    if (respuesta < 1 || respuesta > max) {
+                        mostrar("Error, elige una opción válida [1-" + max + "]: ");
+                    }
+                } while (respuesta < 1 || respuesta > max);
+                return respuesta;
+        }
+
+}
