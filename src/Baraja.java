49ab816bbe7861385b8608f3c8cd84ec0d7a9893
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-05-04 20:07:18 +0200

    Terminada Baraja

--- src/Baraja.java
+++ src/Baraja.java
@@ -1,32 +1,66 @@
 class Baraja {
 
-    void barajar() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private static final int NUM_CARTAS = 52;
+    private final Carta[] CARTAS = new Carta[NUM_CARTAS];
+    private int numeroCartas = NUM_CARTAS;
+
+    public Baraja() {
+        for (int palo = 1; palo <= 4; palo++) {
+            for (int valor = 1; valor <= 13; valor++) {
+                CARTAS[(palo - 1) * 13 + (valor - 1)] = new Carta(palo, valor);
+            }
+        }
     }
 
-    public Object sacarCarta() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sacarCarta'");
+    public void barajar() {
+        for (int i = 0; i < CARTAS.length; i++) {
+            int randomIndex = (int) (Math.random() * CARTAS.length);
+            Carta temp = CARTAS[i];
+            CARTAS[i] = CARTAS[randomIndex];
+            CARTAS[randomIndex] = temp;
+        }
+    }
+
+    public Carta sacarCarta() {
+        if (numeroCartas > 0) {
+            Carta carta = CARTAS[numeroCartas - 1];
+            numeroCartas--;
+            return carta;
+        } else {
+            return null;
+        }
     }
 
     public Carta[] sacarCarta(int numeroCartas) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sacarCarta'");
+        if (numeroCartas > this.numeroCartas) {
+            numeroCartas = this.numeroCartas;
+        }
+        Carta[] cartas = new Carta[numeroCartas];
+        for (int i = 0; i < numeroCartas; i++) {
+            cartas[i] = sacarCarta();
+        }
+        return cartas;
     }
 
     public void ponerCarta(Carta[] cartas) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ponerCarta'");
+        for (Carta carta : cartas) {
+            if (numeroCartas < NUM_CARTAS) {
+                this.CARTAS[numeroCartas] = carta;
+                numeroCartas++;
+            } else {
+                System.out.println("La baraja está llena. No se pueden añadir más cartas.");
+            }
+        }
     }
 
     public boolean estaVacio() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'estaVacio'");
+        return numeroCartas == 0;
     }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        for (int carta = 0; carta < numeroCartas; carta++) {
+            CARTAS[carta].mostrar();
+        }
     }
 
 }
