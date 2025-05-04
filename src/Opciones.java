c141a7ce27fe80d2b66f7279269feaabea7ed6dc
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-05-04 17:19:16 +0200

    Terminado Opciones

--- src/Opciones.java
+++ src/Opciones.java
@@ -1,9 +1,27 @@
 
 public class Opciones {
 
+        String[] opciones = {
+                        "Mover de Baraja a Descarte",
+                        "Mover de Descarte a Palo",
+                        "Mover de Descarte a Columna",
+                        "Mover de Palo a Columna",
+                        "Mover de Columna a Palo",
+                        "Mover de Columna a Columna",
+                        "Voltear carta de Columna",
+                        "Voltear Descarte en Baraja",
+                        "Salir"
+        };
+
         public void imprimir() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'imprimir'");
+                Utilidades.mostrar("Opciones> ");
+                for (int i = 0; i < opciones.length; i++) {
+                        Utilidades.mostrar((i + 1) + ". " + opciones[i]);
+                }
+        }
+
+        public int numeroDeOpciones() {
+                return opciones.length;
         }
 
 }
