d854d83ead027c4cf699e3209e745e78f55000cc
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-05-06 19:42:17 +0200

    Carta
    añadir método voltear()

--- src/Carta.java
+++ src/Carta.java
@@ -1,9 +1,22 @@
 
 public class Carta {
 
-        public Object voltear() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'voltear'");
+        private final String[] PALOS = {"Corazones", "Diamates", "Picas", "Treboles"};
+        private final String[] VALORES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+        private int palo;
+        private int valor;
+        private boolean visible;
+
+        public Carta(int palo, int valor) {
+                this.palo = palo;
+                this.valor = valor;
+
+                visible = false;
+        }
+
+        public void voltear() {
+                visible = !visible;
         }
 
 }
