bcbac2c774bbb64463c9826c55dcf7ca97d2fab6
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-05-06 18:27:00 +0200

    v4

--- src/Jugador.java
+++
@@ -1,9 +0,0 @@
-
-public class Jugador {
-
-        public int elegir(Opciones opciones) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'elegir'");
-        }
-
-}
--- src/Opciones.java
+++
@@ -1,9 +0,0 @@
-
-public class Opciones {
-
-        public void mostrarOpciones() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mostrarOpciones'");
-        }
-
-}
