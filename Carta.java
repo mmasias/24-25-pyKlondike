94bfa3de6e4d7d3ba0b3a8bd4d3188ea6ca98bd7
manuel@aulaInformatica <manuel@masiasweb.com>
2025-05-06 12:59:24 +0200

    Clase Carta finalizada (de momento...)

--- Carta.java
+++ Carta.java
@@ -1,17 +1,48 @@
 public class Carta {
 
-    public Carta(int palo, int numero) {
-        // TODO Auto-generated constructor stub
+    private int palo;
+    private int numero;
+    private boolean bocaArriba;
+
+    private final String[] PALOS = { "♠️", "♣️", "♦️", "♥️" };
+    private final String[] NUMEROS = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+    private Console console;
+
+    public Carta(int unPalo, int unNumero) {
+        palo = unPalo;
+        numero = unNumero;
+        bocaArriba = false;
+        console = new Console();
     }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        String numero = "?";
+        String palo = "?";
+
+        if (bocaArriba) {
+            numero = NUMEROS[this.numero];
+            palo = PALOS[this.palo];
+        }
+        console.write(numero + palo);
     }
 
     public void voltear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'voltear'");
+        bocaArriba = !bocaArriba;
+    }
+
+
+    public static void main(String[] args) {
+        Carta cartaPrueba = new Carta(1,0);
+        System.out.println();
+        cartaPrueba.mostrar();
+        System.out.println();
+        cartaPrueba.voltear();
+        cartaPrueba.voltear();
+        cartaPrueba.voltear();
+        cartaPrueba.mostrar();
+        System.out.println();
+
     }
 
 }
