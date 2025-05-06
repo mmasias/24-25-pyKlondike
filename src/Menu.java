89872e8ae8e757fdf4523368e5d78c27a4f47012
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-05-06 21:56:21 +0200

    Implementar opciones en el menú y completar métodos mostrar y esValida

--- src/Menu.java
+++ src/Menu.java
@@ -1,14 +1,27 @@
 
 public class Menu {
 
+        private final String[] OPCIONES = {
+                        "Mover de Baraja a Descarte",
+                        "Mover de Descarte a Palo",
+                        "Mover de Descarte a Columna",
+                        "Mover de Palo a Columna",
+                        "Mover de Columna a Palo",
+                        "Mover de Columna a Columna",
+                        "Voltear carta de Columna",
+                        "Voltear Descarte en Baraja",
+                        "Salir"
+        };
+
         public void mostrar() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+                System.out.println("OPCIONES>");
+                for (int i = 0; i < OPCIONES.length; i++) {
+                        System.out.println((i + 1) + ". " + OPCIONES[i]);
+                }
         }
 
         public boolean esValida(int respuesta) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'esValida'");
+                return respuesta >= 1 && respuesta <= OPCIONES.length;
         }
 
 }
