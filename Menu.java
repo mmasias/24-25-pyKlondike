4c62d2efadfad8e6e46eafecc5b8d222b18aa44e
manuel@sdf1 <manuel@masiasweb.com>
2025-05-17 00:45:25 +0200

    feat: borrar pantalla al inicio de un turno

--- Menu.java
+++ Menu.java
@@ -2,6 +2,7 @@ public class Menu {
 
     public void imprimeOpciones() {
         Console console = new Console();
+        console.clearScreen();
         console.writeln("OPCIONES>");
         console.writeln("1. Mover de Baraja a Descarte    2. Mover de Descarte a Palo     3. Mover de Descarte a Columna");
         console.writeln("4. Mover de Palo a Columna       5. Mover de Columna a Palo      6. Mover de Columna a Columna");
