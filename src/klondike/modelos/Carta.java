d4f7fca9bf98e9664e8c8cd22190c918845d9880
Manuel Masías <mmasias@users.noreply.github.com>
2025-05-23 10:06:51 +0200

    refactor:mostrar adecuadamente la carta.

--- src/klondike/modelos/Carta.java
+++ src/klondike/modelos/Carta.java
@@ -8,7 +8,7 @@ public class Carta {
     private int numero;
     private boolean bocaArriba;
 
-    private final String[] PALOS = { "♠️", "♣️", "♦️", "♥️" };
+    private final String[] PALOS = { "♠️ ", "♣️ ", "♦️ ", "♥️ " };
     private final String[] NUMEROS = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
     private Console console;
@@ -28,24 +28,11 @@ public class Carta {
             numero = NUMEROS[this.numero];
             palo = PALOS[this.palo];
         }
-        console.write("[" + numero + palo + " ]");
+        console.write("[" + numero + palo + "]");
     }
 
     public void voltear() {
         bocaArriba = !bocaArriba;
-    }
-
-    public static void main(String[] args) {
-        Carta cartaPrueba = new Carta(1, 0);
-        System.out.println();
-        cartaPrueba.mostrar();
-        System.out.println();
-        cartaPrueba.voltear();
-        cartaPrueba.voltear();
-        cartaPrueba.voltear();
-        cartaPrueba.mostrar();
-        System.out.println();
-
     }
 
     public boolean bocaArriba() {
